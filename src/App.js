a48ffb177e2c4f321486170e0558f8465e23068f
Victor Olamide <akinnusivictor098@gmail.com>
2023-02-13 15:31:03 +0100

    Footer and Header Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import ListEmployeeComponent from  './component/ListEmployeeComponent';
+import HeaderComponents from './component/HeaderComponents';
+import FooterComponent from './component/FooterComponent';
 
 function App() {
   return (
-    <div className="container">
-    <ListEmployeeComponent />
+    <div>
+    <HeaderComponents/>
+      <div className="container">
+        <ListEmployeeComponent />
       
+       </div>
+       <FooterComponent/>
     </div>
+
   );
 }
 
